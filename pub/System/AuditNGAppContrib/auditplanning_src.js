4716630de2fafcbfb68433cb8390be0578eda2f7
Stephan Osthold <osthold@modell-aachen.de>
2015-04-13 15:31:55 +0200

    Use Math.round instead of toFixed
    Prince will round 10/3 to 4 when using toFixed().
    
    Maybe Math.floor is more appropriate?
    
    Ticket: #6243

--- pub/System/AuditNGAppContrib/auditplanning_src.js
+++ pub/System/AuditNGAppContrib/auditplanning_src.js
@@ -22,6 +22,6 @@ jQuery(function($) {
             var n = new Number($td.text());
             if(!isNaN(n)) sum += n;
         }
-        $result.text(new Number(sum/$eachtr.length).toFixed());
+        $result.text(Math.round(sum/$eachtr.length));
     });
 });
