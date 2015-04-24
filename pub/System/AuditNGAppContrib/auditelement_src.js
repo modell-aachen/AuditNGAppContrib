e941f5a1df94cbb2df24a0fbbfdaabcea3736897
Stephan Osthold <osthold@modell-aachen.de>
2015-04-24 14:38:25 +0200

    Reworking the 'Add new question' dialog
       * better error handling (extract StrikeOne key)
       * After adding question: show question text, not topic number
       * make dialog abortable
       * no longer cloning the form
    
    TODOs:
       * better StrikeOne handling (you can press 'OK' again and it will
    work, but it still says 'error')
       * nicer display of question
          * best: integrate question into select2

--- pub/System/AuditNGAppContrib/auditelement_src.js
+++ pub/System/AuditNGAppContrib/auditelement_src.js
@@ -1,39 +1,41 @@
 jQuery(function($){
-    $('#addQuestionDialog').on('dialogclose', function() {
-        var $form = $(this).find('form');
-        var $clone = $form.clone();
-        $clone.find('[name="AspectCriteria"]').val($form.find('[name="AspectCriteria"]').val());
-
-        $clone.ajaxForm({
-            beforeSerialize: function($form, options) {
-                if(StrikeOne !== undefined) {
-                    StrikeOne.submit($form[0]);
-                }
-                if($.blockUI !== undefined) $.blockUI();
-            },
-            success: function(data) {
-                if($.blockUI !== undefined) $.unblockUI();
-                var $data = $(data);
-                var created = $data.find('input[name="created"]').val();
-                if(created) {
-                    var $input = $('<input type="text" readonly="readonly" name="Question" />');
-                    $input.val(created);
-                    $('[name="main"] .select2-container').replaceWith($input);
-                }
-                var key = $data.find('input[name="validation_key"]').val();
-                if(key) {
-                    $('input[name="validation_key"]').val(key);
-                }
-            },
-            error: function(data) {
-                if($.blockUI !== undefined) $.unblockUI();
-                alert('An error occured while saving');
-                $('#addQuestionDialog').dialog('open');
+    $('#addQuestionDialog form').ajaxForm({
+        beforeSerialize: function($form, options) {
+            if(StrikeOne !== undefined) {
+                StrikeOne.submit($form[0]);
             }
-        });
-        $clone.submit();
-        //  $form.find('[name="Chapter"]').val('');
-        //  $form.find('[name="AspectCriteria"]').val('');
+            if($.blockUI !== undefined) $.blockUI();
+        },
+        success: function(data) {
+            if($.blockUI !== undefined) $.unblockUI();
+            var $data = $(data);
+            var created = $data.find('input[name="created"]').val();
+            if(created) {
+                var $div = $('<div></div>');
+
+                var $input = $('<input type="hidden" name="Question" />');
+                $input.val(created);
+                $div.append($input);
+
+                var $title = $('<span></span>');
+                $title.html($data.find('.AspectCriteria').html());
+                $div.append($title);
+
+                $('[name="main"] .select2-container').closest('td').empty().append($div);
+            }
+            var key = $data.find('input[name="validation_key"]').val();
+            if(key) {
+                $('input[name="validation_key"]').val(key);
+            }
+            $('#addQuestionDialog').dialog('close');
+        },
+        error: function(data) {
+            if($.blockUI !== undefined) $.unblockUI();
+            var $data = $(data.responseText);
+            var key = $data.find('input[name="validation_key"]').first().val();
+            if(key) $('[name="validation_key"]').val(key);
+            alert('An error occured while saving');
+        }
     });
 
     $('.addQuestion').click(function() {
