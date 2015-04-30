780b4241e2d69bc03607c1f74654c163f27b0e92
Stephan Osthold <osthold@modell-aachen.de>
2015-04-30 15:56:47 +0200

    Make audit questions editable
    Ticket: #6243

--- pub/System/AuditNGAppContrib/auditelement_src.js
+++ pub/System/AuditNGAppContrib/auditelement_src.js
@@ -11,7 +11,7 @@ jQuery(function($){
             var $data = $(data);
             var created = $data.find('input[name="created"]').val();
             if(created) {
-                var $div = $('<div></div>');
+                var $div = $('<div></div>').addClass('question');
 
                 var $input = $('<input type="hidden" name="Question" />');
                 $input.val(created);
@@ -21,7 +21,8 @@ jQuery(function($){
                 $title.html($data.find('.AspectCriteria').html());
                 $div.append($title);
 
-                $('[name="main"] .select2-container').closest('td').empty().append($div);
+                $('[name="main"] .select2-container').closest('.question').replaceWith($div);
+                updateButton();
             }
             var key = $data.find('input[name="validation_key"]').val();
             if(key) {
@@ -41,6 +42,69 @@ jQuery(function($){
     $('.addQuestion').click(function() {
         var $this = $(this);
         $('#addQuestionDialog').dialog('open');
+        return false;
+    });
+
+    var updateButton = function() {
+        var $button = $('.editQuestion');
+        var question = $('input[name="Question"]').val();
+        if(question) {
+            $button.removeClass('disabledButton');
+        } else {
+            $button.addClass('disabledButton');
+        }
+    };
+    $('input[name="Question"]').change(updateButton);
+    updateButton();
+
+    var loadEditDialog = function() {
+        var question = $('input[name="Question"]').val();
+        var $dialogContainer = $('<div></div>').addClass('dialogContainer').hide().appendTo('body');
+        var url = foswiki.getPreference('SCRIPTURL') + '/rest' + foswiki.getPreference('SCRIPTSUFFIX') + '/RenderPlugin/template?render=1;name=AuditElementEdit;expand=editQuestionDialog;question=' + encodeURIComponent(question) + ';topic=' + encodeURIComponent(foswiki.getPreference('WEB').replace(/\//g, '.').replace(/.[^.]*$/, '') + '.DummyTopic');
+        $dialogContainer.load(url, function() {
+            $dialogContainer.find('form').ajaxForm({
+                beforeSerialize: function($form, options) {
+                    if(StrikeOne !== undefined) {
+                        StrikeOne.submit($form[0]);
+                    }
+                    if($.blockUI !== undefined) $.blockUI();
+                },
+                success: function(data) {
+                    if($.blockUI !== undefined) $.unblockUI();
+                    var $data = $(data);
+                    var $div = $('<div></div>').addClass('question');
+
+                    var $input = $('<input type="hidden" name="Question" />');
+                    $input.val(question);
+                    $div.append($input);
+
+                    var $title = $('<span></span>');
+                    $title.html($data.find('.AspectCriteria').html());
+                    $div.append($title);
+
+                    $('[name="main"] .select2-container').closest('.question').replaceWith($div);
+                    updateButton();
+
+                    var key = $data.find('input[name="validation_key"]').val();
+                    if(key) {
+                        $('input[name="validation_key"]').val(key);
+                    }
+                    $('.editQuestionDialog').dialog('close'); // destroy?
+                },
+                error: function(data) {
+                    if($.blockUI !== undefined) $.unblockUI();
+                    var $data = $(data.responseText);
+                    var key = $data.find('input[name="validation_key"]').first().val();
+                    if(key) $('[name="validation_key"]').val(key);
+                    alert('An error occured while saving');
+                }
+            });
+        });
+    };
+
+    $('.editQuestion').click(function() {
+        var $this = $(this);
+        if(!$this.hasClass('disabledButton')) loadEditDialog();
         return false;
     });
 });
